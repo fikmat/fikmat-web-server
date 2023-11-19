4ae62f08e727be8bf7a05972c0d171e22f1410e7
dedekm <dedekmm@gmail.com>
2023-11-19 16:38:26 +0100

    add api/data endpoints, add api usage example

--- public/game1/javascripts/app.js
+++ public/game1/javascripts/app.js
@@ -9,3 +9,28 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+async function getData() {
+  return fetch('http://localhost:3000/api/data/get/game1')
+    .then((response) => response.json())
+    .then((responseJson) => { return responseJson });
+}
+
+async function setData(data) {
+  return fetch('http://localhost:3000/api/data/set/game1', {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then((response) => response.json())
+    .then((responseJson) => { return responseJson });
+}
+
+let data = await getData();
+console.log('current score is ' + data.score);
+
+data.score = Math.round(Math.random() * 100)
+data = await setData(data);
+console.log('score updated to ' + data.score);
